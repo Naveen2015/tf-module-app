98e30f2eb4d2adea4c39dd7840d8c48c2d8df129
naveen <naveenkumar9707@gmail.com>
2024-10-20 12:55:51 -0500

    cname

--- locals.tf
+++ locals.tf
@@ -1,6 +1,6 @@
 locals {
   asg_tags= merge(var.tags,{Name="${var.name}-${var.env}"})
   //dns_name = "${var.name}-${var.env}.${var.domain_name}"
-  dns_name = "${var.dns_name}-${var.domain_name}"
+  dns_name = "${var.dns_name}.${var.domain_name}"
 
 }
